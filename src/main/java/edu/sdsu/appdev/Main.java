b757aca2e067e402c9f6356185facdc0c77a724d
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2024-09-22 00:43:09 -0700

    Fail when incorrect run command is used

--- src/main/java/edu/sdsu/appdev/Main.java
+++ src/main/java/edu/sdsu/appdev/Main.java
@@ -4,10 +4,14 @@ public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Your workflow is not correctly running the Java application. No name argument, Aborting");
+	    
+	    System.exit(-1);
         } else if (args.length == 1) {
             System.out.printf("Congrats. You got the workflow to run successfully, %s!\n", args[0]);
         } else {
             System.out.println("Your workflow is not correctly running the Java application. Too many args (should be 1, your name), aborting");
+
+	    System.exit(-1);
         }
     }
 }
